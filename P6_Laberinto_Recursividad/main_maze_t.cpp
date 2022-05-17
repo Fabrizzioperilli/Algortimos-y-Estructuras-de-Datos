ecf9609b05f035bc4173ca293cbbeb2b24cbdc21
Fabrizzioperilli <alu0101138589@ull.edu.es>
2022-05-17 17:02:56 +0100

    Cambios

---
+++ P6_Laberinto_Recursividad/main_maze_t.cpp
@@ -0,0 +1,36 @@
+// AUTOR: FABRIZZIO DANIELL PERILLI MARTIN  
+// FECHA: 24/05/2022  
+// EMAIL: ALU0101138589@ULL.EDU.ES
+// VERSION: 1.0
+// ASIGNATURA: Algoritmos y Estructuras de Datos
+// PRÁCTICA Nº: 6
+// COMENTARIOS: se indican entre [] las pautas de estilo aplicadas de
+//              "C++ Programming Style Guidelines"
+//              https://geosoft.no/development/cppstyle.html
+// Se compila con:
+// g++ main_maze_t.cpp maze_t.cpp -o main_maze_t
+
+#include <iostream>
+#include "maze_t.hpp"
+
+using namespace std;
+
+int main()
+{ 
+  maze_t M;
+  
+  cin >> M;
+  
+  cout << M << endl;
+  
+  if (M.solve())
+  { 
+    cout << "¡¡ Se ha encontrado una salida al laberinto !!" << endl;
+    cout << M << endl;
+    cout << "Número de pasos: " << M.getCount() << endl;
+  }
+  else
+    cout << "No se ha podido encontrar la salida del laberinto..." << endl;
+  
+  return 1;
+}
